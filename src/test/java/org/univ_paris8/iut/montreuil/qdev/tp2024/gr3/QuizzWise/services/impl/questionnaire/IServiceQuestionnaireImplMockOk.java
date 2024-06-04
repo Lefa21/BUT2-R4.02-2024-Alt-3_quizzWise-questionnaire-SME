68e115e0e3ea87d47a919e9b885289057a69f36d
mirhbira <irhbiram@gmail.com>
2024-06-04 11:14:10 +0200

    bonne version des tests

--- src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr3/QuizzWise/services/impl/questionnaire/IServiceQuestionnaireImplMockOk.java
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr3/QuizzWise/services/impl/questionnaire/IServiceQuestionnaireImplMockOk.java
@@ -11,20 +11,20 @@ import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.QuizzWise.utils.exceptions.
 
 import java.util.ArrayList;
 
+import java.util.ArrayList;
+
 public class IServiceQuestionnaireImplMockOk implements IServiceQuestionnaire {
     @Override
     public QuestionnairesDTO getListQuestionnaires(String sourceFichierCsv) throws FileNotAvailableException, EmptyFileException, CannotReadFileException {
-
-        QuestionDTO questionDTO1 = new QuestionDTO(1,"Question 1", "Reponse 1", Difficulte.DIFFICILE);
-        QuestionDTO questionDTO2 = new QuestionDTO(2, "Question 2", "Reponse 2", Difficulte.FACILE);
-        QuestionDTO questionDTO3 = new QuestionDTO(3,"Question 3", "Reponse 3", Difficulte.INTERMEDIARE);
+        QuestionDTO questionDTO1 = new QuestionDTO(1, "Question 1", "Reponse 1", Difficulte.DIFFICILE);
+        QuestionDTO questionDTO2 = new QuestionDTO(2, "Question 2", "Reponse 2", Difficulte.INTERMEDIARE);
+        QuestionDTO questionDTO3 = new QuestionDTO(3, "Question 3", "Reponse 3", Difficulte.DIFFICILE);
 
         ArrayList<QuestionDTO> listeQuestions = new ArrayList<>();
         listeQuestions.add(questionDTO1);
         listeQuestions.add(questionDTO2);
         listeQuestions.add(questionDTO3);
 
-        QuestionnairesDTO expectedQuestionnaireDTO = new QuestionnairesDTO(listeQuestions);
-        return expectedQuestionnaireDTO;
+        return new QuestionnairesDTO(listeQuestions);
     }
 }
--- src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr3/QuizzWise/services/test/questionnaire/IQuestionnaireTestOk.java
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr3/QuizzWise/services/test/questionnaire/IQuestionnaireTestOk.java
@@ -4,28 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.QuizzWise.entities.dto.QuestionDTO;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.QuizzWise.entities.dto.QuestionnairesDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.QuizzWise.services.impl.questionnaire.IServiceQuestionnaireImplMockOk;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.QuizzWise.services.interfaces.IServiceQuestionnaire;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.QuizzWise.utils.exceptions.enumerate.Difficulte;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.QuizzWise.utils.exceptions.exceptions.CannotReadFileException;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.QuizzWise.utils.exceptions.exceptions.EmptyFileException;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.QuizzWise.utils.exceptions.exceptions.FileNotAvailableException;
 
+
 import java.util.ArrayList;
 
 public class IQuestionnaireTestOk {
 
-    private IServiceQuestionnaire iServiceQuestionnaire  = new IServiceQuestionnaire() {
-        @Override
-        public QuestionnairesDTO getListQuestionnaires(String sourceFichierCsv) throws FileNotAvailableException, EmptyFileException, CannotReadFileException {
-            return null;
-        }
-    };
+    private IServiceQuestionnaire iServiceQuestionnaire = new IServiceQuestionnaireImplMockOk();
 
     @Test
     public void testChargerQuestionnaireOk() throws FileNotAvailableException, EmptyFileException, CannotReadFileException {
         QuestionDTO question1 = new QuestionDTO(1, "Question 1", "Reponse 1", Difficulte.DIFFICILE);
-        QuestionDTO question2 = new QuestionDTO(2,"Question 2", "Reponse 2", Difficulte.INTERMEDIARE);
-        QuestionDTO question3 = new QuestionDTO(3,"Question 3", "Reponse 3", Difficulte.DIFFICILE);
+        QuestionDTO question2 = new QuestionDTO(2, "Question 2", "Reponse 2", Difficulte.INTERMEDIARE);
+        QuestionDTO question3 = new QuestionDTO(3, "Question 3", "Reponse 3", Difficulte.DIFFICILE);
 
         ArrayList<QuestionDTO> listeQuestions = new ArrayList<>();
         listeQuestions.add(question1);
@@ -33,6 +30,8 @@ public class IQuestionnaireTestOk {
         listeQuestions.add(question3);
 
         QuestionnairesDTO expectedQuestionnaireDTO = new QuestionnairesDTO(listeQuestions);
-        Assertions.assertEquals(question1, expectedQuestionnaireDTO.getListQuestionnaires("src/test/resources/QuestionnaireOK").getListeQuestions().get(0));
+        QuestionnairesDTO actualQuestionnaireDTO = iServiceQuestionnaire.getListQuestionnaires("src/test/resources/QuestionnaireOK");
+
+        Assertions.assertEquals(expectedQuestionnaireDTO, actualQuestionnaireDTO);
     }
 }
